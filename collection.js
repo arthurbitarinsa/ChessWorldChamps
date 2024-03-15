4efefde1dda8898fa1c54a4a4920e53609f0418f
arthurbitarinsa <bitarinsaarthur2@gmail.com>
2024-03-15 14:56:54 +0100

    ChessChamps

---
+++ collection.js
@@ -0,0 +1,75 @@
+const collection = [
+    {
+      name: 'DING LIREN',
+      country : 'CHINA',
+      years : 2020-present,
+      rating: 2816
+    },
+
+    {
+        name: 'MAGNUS CARLSEN',
+        country : 'Norway',
+        years : 2013-2023,
+        rating: 2882
+    },
+
+      {
+        name: 'VISWANATHAN ANAND',
+        country : 'India',
+        years : 2007-2013,
+        rating: 2817
+      },
+
+      {
+        name: 'VLADIMIR KRAMNIK',
+        country : 'Russia',
+        years : 2006-2007,
+        rating: 2817
+      },
+
+      {
+        name: 'GARY KASPAROV',
+        country : 'Russia',
+        years : 1985-2000,
+        rating: 2851
+      },
+
+      {
+        name: 'ANATOLY KARPOV',
+        country : 'Russia',
+        years : 1975-1985,
+        rating: 2780
+      },
+
+      {
+        name: 'BOBBY FISCHER',
+        country : 'USA',
+        years : 1972-1975,
+        rating: 2785
+      },
+
+      {
+        name: 'BORIS SPASSKY',
+        country : 'RUSSIA',
+        years : 1969-1972,
+        rating: 2690
+      },
+
+      {
+        name: 'TIGRAN PETROSIAN',
+        country : 'Soviet/Georgia',
+        years : 1963-1969,
+        rating: 2645
+      },
+
+      {
+        name: 'MIKHAIL TAL',
+        country : 'Uganda',
+        years : 2024,
+        rating: 2705    
+      },
+  
+  ]
+
+
+
---
+++ script.js
@@ -0,0 +1,21 @@
+/*
+const header = document.createElement("header");
+const r_corner = document.createElement("div");
+r_corner.setAttribute("class", "round-corner");
+const head_text = document.createElement("div");
+
+const head_h1 = document.createElement("h1");
+head_h1.textContent = "10 CHESS WORLD CHAMPIONS";
+
+const head_p = document.createElement("p");
+head_p.textContent = 
+`
+The list below depicts 10 previous chess world CHAMPIONS
+that have existed over the years, it will describe their 
+brpersonal detais and years of their activity and Championships.`
+
+head_text.append(head_h1,head_p);
+header.append(r_corner,head_text);
+
+document.body.appendChild(header);
+ */
